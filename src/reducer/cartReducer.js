4de9461a22eb02447a6c848d8390db4c3e31215b
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-07-10 02:51:33 +0530

    Frontend done

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -104,58 +104,71 @@ const cartReducer = (state, action) => {
     };
   }
 
-  if (action.type === "CART_TOTAL_ITEM") {
-    let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
-      let { amount } = curElem;
-
-      initialVal = initialVal + amount;
-      return initialVal;
-    }, 0);
-
-    return {
-      ...state,
-      total_item: updatedItemVal,
-    };
-  }
-
-  if (action.type === "CART_TOTAL_PRICE") {
-    let total_price = state.cart.reduce((initialVal, curElem) => {
-      let { price, amount } = curElem;
-
-      initialVal = initialVal + price * amount;
-      // 25000 + 0 = 25000
-      // 10199 + 25000 = 121
-
-      return initialVal;
-    }, 0);
-
-    return {
-      ...state,
-      total_price,
-    };
-  }
-
-  // if (action.type === "CART_ITEM_PRICE_TOTAL") {
-  //   let { total_item, total_price } = state.cart.reduce(
-  //     (accum, curElem) => {
-  //       let { price, amount } = curElem;
-
-  //       accum.total_item += amount;
-  //       accum.total_price += price * amount;
-
-  //       return accum;
-  //     },
-  //     {
-  //       total_item: 0,
-  //       total_price: 0,
-  //     }
-  //   );
+  // if (action.type === "CART_TOTAL_ITEM") {
+  //   if (!state.cart) {
+  //     return state; // Return the current state if cart is null
+  //   }
+  
+  //   let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+  //     let { amount } = curElem;
+  
+  //     initialVal = initialVal + amount;
+  //     return initialVal;
+  //   }, 0);
+  
   //   return {
   //     ...state,
-  //     total_item,
+  //     total_item: updatedItemVal,
+  //   };
+  // }
+  
+
+  // if (action.type === "CART_TOTAL_PRICE") {
+  //   if (!state.cart) {
+  //     return state; // Return the current state if cart is null
+  //   }
+    
+  //   let total_price = state.cart.reduce((initialVal, curElem) => {
+  //     let { price, amount } = curElem;
+
+  //     initialVal = initialVal + price * amount;
+  //     // 25000 + 0 = 25000
+  //     // 10199 + 25000 = 121
+
+  //     return initialVal;
+  //   }, 0);
+
+  //   return {
+  //     ...state,
   //     total_price,
   //   };
   // }
+
+  if (action.type === "CART_ITEM_PRICE_TOTAL") {
+    if (!state.cart) {
+      return state; 
+    }
+
+    let { total_item, total_price } = state.cart.reduce(
+      (accum, curElem) => {
+        let { price, amount } = curElem;
+
+        accum.total_item += amount;
+        accum.total_price += price * amount;
+
+        return accum;
+      },
+      {
+        total_item: 0,
+        total_price: 0,
+      }
+    );
+    return {
+      ...state,
+      total_item,
+      total_price,
+    };
+  }
 
   return state;
 };
